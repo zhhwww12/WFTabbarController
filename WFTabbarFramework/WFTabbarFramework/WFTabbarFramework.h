c4d0d85745e15a72235ee4626dc60cf42d830998
我就是小伟哥 <zhhwww1@foxmail.com>
2019-08-01 11:27:53 +0800

    添加类
    1

---
+++ WFTabbarFramework/WFTabbarFramework/WFTabbarFramework.h
@@ -0,0 +1,23 @@
+//
+//  WFTabbarFramework.h
+//  WFTabbarFramework
+//
+//  Created by 仁和 on 2019/8/1.
+//  Copyright © 2019 wfw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WFTabbarFramework.
+FOUNDATION_EXPORT double WFTabbarFrameworkVersionNumber;
+
+//! Project version string for WFTabbarFramework.
+FOUNDATION_EXPORT const unsigned char WFTabbarFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WFTabbarFramework/PublicHeader.h>
+
+
+#import <WFTabbarFramework/WFTabbarController.h>
+#import <WFTabbarFramework/WFNavgationViewController.h>
+
+
